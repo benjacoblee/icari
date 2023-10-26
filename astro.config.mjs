22ff784b1fc4b569406c9339762b14122d64c97f
benjacoblee <benjacoblee@gmail.com>
2023-10-27 00:22:37 +0800

    :wrench: Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-cube.vercel.app/',
   integrations: [sitemap()],
+  output: 'server',
+  adapter: netlify(),
 });
